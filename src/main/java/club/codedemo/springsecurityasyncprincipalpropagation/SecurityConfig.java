d965ee2dc966c6a60ec79e5bbdef1427365b79fe
panjie <3792535@qq.com>
2020-08-05 14:32:58 +0800

    init

--- src/main/java/club/codedemo/springsecurityasyncprincipalpropagation/SecurityConfig.java
+++ src/main/java/club/codedemo/springsecurityasyncprincipalpropagation/SecurityConfig.java
@@ -3,11 +3,13 @@ package club.codedemo.springsecurityasyncprincipalpropagation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -18,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
             .withUser("admin")
-            .password(this.passwordEncoder().encode("admin"));
+            .password(this.passwordEncoder().encode("admin"))
+            .roles("");
     }
 }

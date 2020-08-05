98d135460fb9695a2be3125f1827fc0db38f3e6c
panjie <3792535@qq.com>
2020-08-05 14:12:34 +0800

    init

---
+++ src/main/java/club/codedemo/springsecurityasyncprincipalpropagation/SpringSecurityAsyncPrincipalPropagationApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.springsecurityasyncprincipalpropagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAsyncPrincipalPropagationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAsyncPrincipalPropagationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/springsecurityasyncprincipalpropagation/SpringSecurityAsyncPrincipalPropagationApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.springsecurityasyncprincipalpropagation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAsyncPrincipalPropagationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
